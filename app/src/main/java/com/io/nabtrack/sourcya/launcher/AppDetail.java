51bae5c4a806158a46f80e2fb98395f82d508593
m-ismail <mohamedismail77@yahoo.com>
2018-02-15 15:14:30 +0200

    Initial commit

---
+++ app/src/main/java/com/io/nabtrack/sourcya/launcher/AppDetail.java
@@ -0,0 +1,14 @@
+package com.io.nabtrack.sourcya.launcher;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by m-ismail on 01/02/18.
+ */
+
+public class AppDetail {
+
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
---
+++ app/src/main/java/com/io/nabtrack/sourcya/launcher/HomeActivity.java
@@ -0,0 +1,46 @@
+package com.io.nabtrack.sourcya.launcher;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.ProgressBar;
+
+/**
+ * Created by m-ismail on 01/02/18.
+ */
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        final ProgressBar progressBar = findViewById(R.id.loading);
+
+        new CountDownTimer(3000,5) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                progressBar.setProgress((int) (3000-millisUntilFinished));
+
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(HomeActivity.this, AppsListActivity.class));
+
+            }
+        }.start();
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        
+    }
+}
